2ae14f0b719b14fc6ce517a6c29b9dd4cd0f0c70
Francesco Delacqua <francesco.delacqua@gmail.com>
2021-05-12 09:48:21 -0500

    bugfix

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -3,6 +3,6 @@ namespace Vanderbilt\AdvancedImport\App;
 
 $app_root = dirname(dirname(__FILE__));
 $redcap_root = dirname(dirname($app_root));
-require_once join([$app_root,'vendor','autoload.php'],DIRECTORY_SEPARATOR);
+require_once join(DIRECTORY_SEPARATOR, [$app_root,'vendor','autoload.php']);
 
 error_reporting(E_ALL);
